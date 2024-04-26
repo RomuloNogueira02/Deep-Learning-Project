1151b6ba398458a40c44655f8c85c63401d93a96
Rómulo <romulo3218@gmail.com>
2024-04-26 20:31:01 +0100

    add new version of the Dataset where include all the images with or without label

---
+++ poolDatasetV2.py
@@ -0,0 +1,86 @@
+import os
+import xml.etree.ElementTree as ET
+from PIL import Image
+from torch.utils.data import Dataset
+import torch
+import matplotlib.pyplot as plt
+import cv2
+from helpers import *
+from torchvision.transforms import v2, PILToTensor
+
+class PoolDatasetV2(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+
+        self.labeled_images = get_images_labeled(self.root_dir.replace('images','labels'))
+        self.retrieve_data()
+
+    def retrieve_data(self):
+        self.images = []
+        self.labels = []
+        
+        for file in os.listdir(self.root_dir):
+            
+            full_image_path = self.root_dir + "\\" + file
+            label_name = change_image_to_label(file)
+
+
+            # Deal with the image
+            img = Image.open(full_image_path).convert('RGB')
+
+            # Transform to tensor
+            if "PIL" in str(type(img)):
+                img = PILToTensor()(img)
+            else:
+                img = torch.Tensor(img)
+
+            self.images.append(img)
+
+
+            # Deal with the label
+            if label_name in self.labeled_images:
+                boxes = getBoxes(self.root_dir.replace('images','labels') + "\\" + label_name)
+                self.labels.append([torch.Tensor(box) for box in boxes])
+
+            else:
+                self.labels.append(torch.Tensor([]))
+
+    def __len__(self):
+        return len(self.images)
+    
+
+    def __getitem__(self, idx):
+        img = self.images[idx]
+        boxes = self.labels[idx]
+
+        old_width = img.shape[1]
+
+        if self.transform:
+            img = torch.Tensor(self.transform(img))
+            
+            factor = old_width / img.shape[1]
+
+            if len(boxes) > 0:
+                boxes = list(map(lambda l: torch.tensor([v / factor for v in l]), boxes))
+
+        else:
+            boxes = torch.Tensor(boxes)
+
+        return img, boxes
+
+
+# EXAMPLE
+# transforms = v2.Compose([
+#     v2.Resize((224, 224)),
+#     v2.ToImage(),
+#     #v2.RandomHorizontalFlip(p=1),
+#     v2.ToDtype(torch.float32, scale=True),
+#     #v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+# ])
+
+
+# ds = PoolDatasetV2(".\\dataset\\images", transform=transforms)
+# # ds = PoolDatasetV2(".\\dataset\\images", transform=None)
+# item = ds[7]
+# print(item[1])
