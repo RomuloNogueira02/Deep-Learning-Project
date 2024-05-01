687ebb4d8ad19a92b4c5de4be2db59ed73bb4f17
Guilherme Ribeiro <80457586+Gui921@users.noreply.github.com>
2024-05-01 12:16:40 +0100

    Added function to divide in Train and Test

--- poolDatasetV2.py
+++ poolDatasetV2.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import cv2
 from helpers import *
 from torchvision.transforms import v2, PILToTensor
+from torch.utils.data import random_split
 
 class PoolDatasetV2(Dataset):
     def __init__(self, root_dir, transform=None):
@@ -63,11 +64,19 @@ class PoolDatasetV2(Dataset):
 
             if len(boxes) > 0:
                 boxes = list(map(lambda l: torch.tensor([v / factor for v in l]), boxes))
-
+                #max_boxes = max(len(boxes), 1)  # Ensure at least 1 box
+                #pad_boxes = F.pad(torch.stack(boxes), (0, 0, 0, max_boxes - len(boxes)), value=-1)
+                #boxes = pad_boxes.unbind(0)
         else:
             boxes = torch.Tensor(boxes)
 
         return img, boxes
+    
+    def split_Data(self, n_test=0.33):
+        test_size = round(n_test * len(self.images))
+        train_size = len(self.images) - test_size
+
+        return random_split(self, [train_size, test_size])
 
 
 # EXAMPLE
